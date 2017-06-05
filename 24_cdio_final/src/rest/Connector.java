ad6491eeb748b6f4f9f03363576e349898cf510c
Einar Logi <einarlogi1@gmail.com>
2017-06-05 13:47:11 +0200

    got web server up and running

--- 24_cdio_final/src/rest/Connector.java
+++ 24_cdio_final/src/rest/Connector.java
@@ -1,6 +1,18 @@
 package rest;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/bla")
 public class Connector {
 	
+	@GET
+	@Path("/hello")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String hello() {
+		return "Hello from backend";
+	}
 	
 }
