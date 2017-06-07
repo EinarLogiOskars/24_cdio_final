f8a122d220ae897851ef50928b3d3f71305f9810
sammy <sammy@LAPTOP-NMHSVPU8.eduroam.wireless.dtu.dk>
2017-06-07 15:08:22 +0200

    Foreman udvidelse

---
+++ 24_cdio_final/WebContent/Foreman/foremanIndex.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+	
+	$("#createMB").on("click", function() {
+		$("#div2").load("createMB.html");
+	});
+
+	$("#showMB").click(function() {
+		$("#div2").load("showMB.html");
+	});	
+
+	$("#createPB").click(function() {
+		$("#div2").load("createPB.html");
+	});
+	
+	$("#showPB").click(function() {
+		$("#div2").load("showPB.html")
+	});
+	
+	$("#exitbtn").click(function() {
+		window.location.assign("../Home/home.html");
+	});
+
+//	$("#showusers").on("click", function(){
+//		$.ajax({
+//			type: 'GET',
+//			url: "http://localhost:8080/24_cdio3/rest2/cdio3/showusers",
+//			success: function(data) {
+//				$("#div2").load("showUsers.html");
+//			}, 
+//			error: function(){
+//				alert("error");
+//			}
+//		});
+//	});
+});
